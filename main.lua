1dfea34f403b4c953d30e6b77aca589bcd7a7cad
Annamaria Orban <anna.orban@yahoo.com>
2019-02-24 22:26:31 +0000

    load images

--- main.lua
+++ main.lua
@@ -1,5 +1,10 @@
 function love.load()
-
+    images = {}
+    for imageIndex, images in ipairs({
+        1, 2, 3, 4, 5, 6, 7, 8,'uncovered', 'covered_highlighted', 'covered', 'flower', 'flag', 'question',
+    }) do
+        images[image] = love.graphics.newImage('images/'..image..'.png')
+    end
 end
 
 function love.update()
