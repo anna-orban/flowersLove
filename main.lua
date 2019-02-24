4899e79a6476f30f597b90638503659449494134
Annamaria Orban <anna.orban@yahoo.com>
2019-02-24 22:31:10 +0000

    draw grid

--- main.lua
+++ main.lua
@@ -1,10 +1,13 @@
 function love.load()
     images = {}
-    for imageIndex, images in ipairs({
+    for imageIndex, image in ipairs({
         1, 2, 3, 4, 5, 6, 7, 8,'uncovered', 'covered_highlighted', 'covered', 'flower', 'flag', 'question',
     }) do
         images[image] = love.graphics.newImage('images/'..image..'.png')
     end
+    cellSize = 18
+    gridX, gridY = love.graphics.getWidth() / cellSize, love.graphics.getHeight() / cellSize
+
 end
 
 function love.update()
@@ -12,5 +15,9 @@ function love.update()
 end
 
 function love.draw()
-
+    for y = 1, gridY do
+        for x = 1, gridX do
+            love.graphics.draw(images.covered, (x - 1) * cellSize, (y - 1) * cellSize)
+        end
+    end   
 end
